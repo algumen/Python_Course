5c1593e173375dca108e40221e82b4d1382f702f
algumen <algumen@gmail.com>
2017-01-26 03:58:40 +0200

    from PyCharm

--- 11_oleksii_webdriver_launch.py
+++ 11_oleksii_webdriver_launch.py
@@ -8,3 +8,5 @@ search_field = chrome_page.find_element_by_id('searchInput')
 search_field.send_keys('список римських пап')
 search_button =chrome_page.find_element_by_id('searchButton').click()
 search_image = chrome_page.find_element_by_css_selector("[alt='3-St.Cletus.jpg']").click()
+
+#change to test commit
