38ba72fc60c07f44e1f3151dc898936e1bf7d245
algumen <algumen@gmail.com>
2017-02-14 20:45:51 +0200

    from PyCharm

---
+++ PyTest/test_easy.py
@@ -0,0 +1,13 @@
+import pytest
+#class TestEasyStart():
+
+def summarise(a,b):
+    return a+b
+
+
+def test_easy_start_2x2():
+    assert summarise(2, 2)==4
+
+
+def test_easy_start_2x2_fail():
+    assert summarise(2, 2)==5
---
+++ PyTest/test_wiki_anaklitus.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from datetime import datetime
+from time import gmtime, strftime
+import pytest
+
+
+class TestWikiAnaklitus():
+    def test_open_image_anaklitus(self):
+        print('*********************************************************')
+        print(datetime.now())
+        wiki_page = webdriver.Chrome()
+        wiki_page.get('https://uk.wikipedia.org')
+        #assert 'Вікіпедія' in wiki_page.title
+
+        wiki_page.find_element_by_id('searchInput').send_keys('список римських пап')
+        wiki_page.find_element_by_id('searchButton').click()
+        wiki_page.find_element_by_css_selector("[alt='3-St.Cletus.jpg']").click()
+
+        assert 'Cletus' in wiki_page.find_element_by_css_selector(r"[href='https://en.wikipedia.org/wiki/Pope_Cletus']")
+
+
+'''
+        search_field = c
+        search_field.send_keys('список римських пап')
+        search_button =wiki_page.find_element_by_id('searchButton').click()
+        search_image = wiki_page.find_element_by_css_selector("[alt='3-St.Cletus.jpg']").click()
+'''
